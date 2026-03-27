416aac83e6ea921c0bdee114b799456072daae5d
Ucrash <gducrash@gmail.com>
2026-03-28 00:53:05 +0200

    begin working on explore page

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import type { Route } from 'react-native-tab-view';
 
 import { useChangeAppStateOnFocus } from '@/hooks/useChangeAppStateOnFocus';
+import Tabs from '@/components/general/Tabs';
+
+const TAB_ROUTES = [
+    { key: 'projects', title: 'Projects' },
+    { key: 'studios', title: 'Studios' },
+];
 
 const ExplorePage = () => {
+
+    const insets = useSafeAreaInsets();
+
+    const [tabIndex, setTabIndex] = useState(0);
 
     useChangeAppStateOnFocus({
         headerVisible: true,
@@ -11,9 +23,20 @@ const ExplorePage = () => {
         primaryColor: 'explore',
     });
 
+    const renderScene = ({ route }: { route: Route }) => <Text>{route.key}</Text>;
+
     return (
         <View style={styles.container}>
-            <Text>Explore</Text>
+            <View style={[styles.pageStart, { paddingTop: insets.top + 82 }]}>
+                <Text style={styles.headingText}>Explore</Text>
+            </View>
+            <Tabs 
+                routes={TAB_ROUTES}
+                currentTab={tabIndex}
+                onTabChange={setTabIndex}
+                renderScene={renderScene}
+                variation='explore'
+            />
         </View>
     );
 };
@@ -23,8 +46,22 @@ export default ExplorePage;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
         backgroundColor: '#121212',
+    },
+
+    pageStart: {
+        backgroundColor: '#183729',
+        padding: 16,
+        zIndex: 2,
+        width: '100%',
+    },
+
+    headingText: {
+        fontSize: 28,
+        fontWeight: 900,
+        color: '#fff',
+        textAlign: 'center',
     },
 });
